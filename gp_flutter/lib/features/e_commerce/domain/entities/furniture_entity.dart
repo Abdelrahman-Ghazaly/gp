5b0d42dec8ee80079a6d09db040966910d3972e1
Abdelrahman Ghazaly <ghazaly.abdelrahman@gmail.com>
2023-03-28 21:43:15 +0200

    .

--- gp_flutter/lib/features/e_commerce/domain/entities/furniture_entity.dart
+++ gp_flutter/lib/features/e_commerce/domain/entities/furniture_entity.dart
@@ -2,18 +2,18 @@ class FurnitureEntity {
   final int id;
   final String title;
   final String description;
-  final String imgUrl;
+  final String imageUrl;
   final String category;
   final String price;
-  final String userId;
+  final String sellerId;
 
   FurnitureEntity({
     required this.id,
     required this.title,
     required this.description,
-    required this.imgUrl,
+    required this.imageUrl,
     required this.category,
     required this.price,
-    required this.userId,
+    required this.sellerId,
   });
 }
