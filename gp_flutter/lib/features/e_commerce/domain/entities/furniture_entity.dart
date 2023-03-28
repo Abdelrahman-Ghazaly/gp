fbe90e30aee25961d44355b7ca5d869b14e78a1b
Abdelrahman Ghazaly <ghazaly.abdelrahman@gmail.com>
2023-03-28 21:07:36 +0200

    added FurnitureEntity

---
+++ gp_flutter/lib/features/e_commerce/domain/entities/furniture_entity.dart
@@ -0,0 +1,19 @@
+class FurnitureEntity {
+  final int id;
+  final String title;
+  final String description;
+  final String imgUrl;
+  final String category;
+  final String price;
+  final String userId;
+
+  FurnitureEntity({
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.imgUrl,
+    required this.category,
+    required this.price,
+    required this.userId,
+  });
+}
