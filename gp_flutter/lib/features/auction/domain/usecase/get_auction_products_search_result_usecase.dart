64d219a1eb0780b364dc44d384822b97ec6fd83a
osama-abdelrassoul <osamaabdelrassoul@gmail.comm>
2023-03-29 23:59:29 +0200

    update

---
+++ gp_flutter/lib/features/auction/domain/usecase/get_auction_products_search_result_usecase.dart
@@ -0,0 +1,17 @@
+import 'package:dartz/dartz.dart';
+import 'package:gp_flutter/core/error/failure.dart';
+import 'package:gp_flutter/core/use_case/use_case.dart';
+import 'package:gp_flutter/features/auction/domain/entities/auction_product.dart';
+import 'package:gp_flutter/features/auction/domain/repository/base_auction_repository.dart';
+
+class GetAuctionProductsSearchResultUseCase
+    extends BaseUseCase<List<AuctionProduct>, NoParams> {
+  final BaseAuctionRepository baseAuctionRepository;
+
+  GetAuctionProductsSearchResultUseCase(this.baseAuctionRepository);
+
+  @override
+  Future<Either<Failure, List<AuctionProduct>>> call(NoParams params) {
+    return baseAuctionRepository.getAuctionProductsSearchResult();
+  }
+}
