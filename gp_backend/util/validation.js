bd85d6cb060f482922380c43ba2a95f6f1c2df01
AhmedMagdy79 <ahmedmagdy35731@gmail.com>
2023-04-03 22:56:28 +0200

    .

--- gp_backend/util/validation.js
+++ gp_backend/util/validation.js
@@ -8,10 +8,10 @@ const validatior = (schema) => {
 
 const userSchema = joi.object({
     email: joi.string().email().required(),
-    name: joi.string().min(3).max(15).required(),
-    password: joi.string().min(5).max(10).required(),
+    name: joi.string().min(3).required(),
+    password: joi.string().min(5).required(),
     confirmPassword: joi.any().valid(joi.ref("password")).required(),
-    address: joi.string().min(5).max(50).required(),
+    address: joi.string().min(5).required(),
     phone: joi.string().required(),
 });
 
